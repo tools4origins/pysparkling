64b389b8ff7f9bb04d939e3eb82f1c3eaa54e855
tools4origins <tools4origins tools4origin@gmail.com>
2019-04-07 20:41:56 +0200

    fix RDD.reduce when rdd contain empty partitions

--- pysparkling/rdd.py
+++ pysparkling/rdd.py
@@ -1033,14 +1033,34 @@ class RDD(object):
         Example:
 
         >>> from pysparkling import Context
-        >>> Context().parallelize([0, 4, 7, 4, 10]).reduce(lambda a, b: a+b)
+        >>> Context().parallelize([0, 4, 7, 4, 10], 2).reduce(lambda a, b: a+b)
         25
+        >>> Context().parallelize([0, 4, 7, 4, 10], 10).reduce(lambda a, b: a+b)
+        25
+        >>> Context().parallelize([0], 10).reduce(lambda a, b: a+b)
+        0
         """
-        return self.context.runJob(
+        _empty = object()
+
+        def result_handler(intermediate_results):
+            try:
+                return functools.reduce(f, (x for x in intermediate_results if x is not _empty))
+            except TypeError as e:
+                if e.args[0] == "reduce() of empty sequence with no initial value":
+                    return _empty
+                else:
+                    raise e
+
+        result = self.context.runJob(
             self,
-            lambda tc, x: functools.reduce(f, x),
-            resultHandler=lambda x: functools.reduce(f, x),
+            lambda tc, x: result_handler(x),
+            resultHandler=result_handler
         )
+
+        if result is _empty:
+            raise ValueError("Can not reduce() empty RDD")
+
+        return result
 
     def reduceByKey(self, f, numPartitions=None):
         """reduce by key
