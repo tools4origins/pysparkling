69148f802ef1e2ad189d3830c8de1e18ebb45a7f
tools4origins <tools4origins tools4origin@gmail.com>
2019-05-17 00:28:08 +0200

    Add meanApprox method to RDD

--- pysparkling/rdd.py
+++ pysparkling/rdd.py
@@ -930,6 +930,7 @@ class RDD(object):
     def mean(self):
         """returns the mean of this dataset
 
+        :rtype: float
 
         Example:
 
@@ -938,6 +939,13 @@ class RDD(object):
         5.0
         """
         return self.stats().mean()
+
+    def meanApprox(self):
+        """same as :func:`~pysparkling.RDD.mean()`
+
+        :rtype: float
+        """
+        return self.mean()
 
     def min(self):
         """returns the minimum element"""
