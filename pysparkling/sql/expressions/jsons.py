081509b93f206c5bf39b7296a66870b52509c50a
tools4origins <tools4origins@gmail.com>
2020-08-17 18:40:51 +0200

    Set JSON function option default values

--- pysparkling/sql/expressions/jsons.py
+++ pysparkling/sql/expressions/jsons.py
@@ -2,24 +2,23 @@ import json
 
 from pysparkling.sql.casts import get_time_formatter
 from pysparkling.sql.expressions.expressions import Expression
+from pysparkling.sql.internal_utils.options import Options
 from pysparkling.utils import get_json_encoder
 
 
 class StructsToJson(Expression):
+    default_options = dict(
+        dateFormat="yyyy-MM-dd",
+        timestampFormat="yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
+    )
+
     def __init__(self, column, options):
         super(StructsToJson, self).__init__(column)
         self.column = column
-        self.options = options
-        if options:
-            date_format = options.get("dateformat", "yyyy-MM-dd")
-            timestamp_format = options.get("timestampformat", "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
-        else:
-            date_format = "yyyy-MM-dd"
-            timestamp_format = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
-        self.encoder = get_json_encoder(
-            date_formatter=get_time_formatter(date_format),
-            timestamp_formatter=get_time_formatter(timestamp_format)
-        )
+        from pysparkling.sql.internal_utils.readers.jsonreader import JSONReader
+        self.input_options = options
+        self.options = Options(JSONReader.default_options, options)
+        self.encoder = get_json_encoder(self.options)
 
     def eval(self, row, schema):
         value = self.column.eval(row, schema)
@@ -32,7 +31,7 @@ class StructsToJson(Expression):
     def __str__(self):
         return "structstojson({0}{1})".format(
             self.column,
-            ", {0}".format(self.options) if self.options is not None else ""
+            ", {0}".format(self.input_options) if self.input_options is not None else ""
         )
 
 
